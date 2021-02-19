034e1f1655c53a7b10971a78488c3b4a34f4e9a9
Ishant Dahiya <ishantdahiya@gmail.com>
2021-02-19 22:34:43 +0530

    basic chat app done

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.getElementById('chat-container');
+const socket = io();
+
+// Message retrieval from server
+socket.on('message', message => {
+    outputMessage(message);
+
+    //Scroll Down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//When user sends a message
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+
+    //Send message to server
+    socket.emit('chatMessage', msg);
+
+    //Clearing input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+// Output message to DOM 
+
+function outputMessage(message) {
+    const li = document.createElement('li');
+    li.classList.add('chat-left');
+    li.innerHTML = `<div class="chat-avatar">
+                        <div class="chat-name">Russell</div>
+                    </div>
+                    <div class="chat-text">${message}</div>
+                    <div class="chat-hour">08:57 <span class="fa fa-check-circle"></span></div>`;
+    document.getElementById('chat-box').appendChild(li);
+}
--- server.js
+++ server.js
@@ -10,7 +10,21 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-    console.log("New Web Socket Connection");
+    socket.emit('message', 'Welcome to Friendle');
+
+    // Send message when any user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Send message when any user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    //Listen for chat messages
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 });
 
 
