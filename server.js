a38ef079083f4159f7dcc37abb7b17a4e5945d87
Ishant Dahiya <ishantdahiya@gmail.com>
2021-02-20 00:50:46 +0530

    heroku change

--- server.js
+++ server.js
@@ -55,7 +55,4 @@ io.on('connection', socket => {
     });
 });
 
-
-const PORT = 3000 || process.env.PORT;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`) );
+server.listen(process.env.PORT || 5000);
